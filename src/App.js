6f160d7da998fce0b15e191fea2661155679ba95
choitjddn0311 <cswcjw311427@naver.com>
2023-09-13 18:44:16 +0900

    react coding apple 4th

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  let [글제목, b] = useState(['남자 코트 추천','여자 코트 추천' , '감자맛집', '2023.09.13 코딩애플 리엑트 4강부터','CSW blog'])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <h4>{글제목[4]}</h4>
+      </div>
+      <div className='list'>
+        <h4>{글제목[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <h4>{ 글제목[3]}</h4>
     </div>
   );
 }
